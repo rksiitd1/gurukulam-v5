cbc1c898a3ace724e1cf5d13b342a7029bb26f28
Ratnesh Kumar Sharma <rksiitd123@gmail.com>
2025-06-25 17:24:34 +0530

    Update mentors.tsx

--- components/about/mentors.tsx
+++ components/about/mentors.tsx
@@ -94,7 +94,7 @@ export function Mentors() {
   }
 
   return (
-    <section className="py-20 bg-white">
+    <section id="mentors" className="py-20 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Section Header */}
         <div className="text-center mb-16">
